73000bdcdf7ca7e36824209cd7ba0e6371a6f03a
bchuup <bchuup@gmail.com>
2020-01-23 13:09:02 -0500

    prepping for build

---
+++ server/public/precache-manifest.60d6b396420de622fa2a13e98af20600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909f0df793467cf3dff8f1d342d5012c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90f38fa8ec78d27075c6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bcdcadff6536e80ba4dc",
+    "url": "/static/js/2.c9d68765.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c9d68765.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90f38fa8ec78d27075c6",
+    "url": "/static/js/main.72d810b3.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
