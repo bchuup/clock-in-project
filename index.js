f0db3e50b4b2017997b356cfabc0e2048aadf1e8
bchuup <bchuup@gmail.com>
2020-01-23 18:28:32 -0500

    add blank endpoint

--- index.js
+++ index.js
@@ -32,15 +32,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use(bodyParser.json());
 app.use(cors());
+app.use('/', (res) => {
+  res.end();
+});
 app.use('/api', apiRoutes);
 
-// app.get('*', (req, res) => {
-//   if (process.env.NODE_ENV === 'production') {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-//   } else {
-//     res.end()
-//   }
-// });
+app.get('*', (req, res) => {
+  if (process.env.NODE_ENV === 'production') {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  } else {
+    res.end()
+  }
+});
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
