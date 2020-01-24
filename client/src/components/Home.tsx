515b9883cb402db342298e3dda954ae31535e5ea
bchuup <bchuup@gmail.com>
2020-01-24 00:16:21 -0500

    can almost make new user

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { ButtonBase, Input } from '@material-ui/core';
+import { ButtonBase, Input, Dialog, DialogContent, DialogTitle, DialogActions } from '@material-ui/core';
 import styled from 'styled-components';
 import http from '../http';
 import { User } from '../models';
+import { useHistory } from 'react-router-dom';
 
 const UserButton = styled(ButtonBase)`
-  min-width: 400px;
+  min-width: 300px;
+  min-height: 48px;
   &&:hover {
     transform: scale(1.1);
   }
   && {
+    padding: 0em 1em;
     margin: 1em;
     border-radius: 10px;
     border: 1px solid #54494B;
@@ -36,15 +39,40 @@ interface Props {
 }
 
 const Home: React.FunctionComponent<Props> = ({ users }) => {
+  const [newName, setNewName] = useState('');
   const [homeUsers, setHomeUsers] = useState<User[]>([]);
+  const [dialogIsOpen, setDialogOpen] = useState(false);
+  const history = useHistory();
   useEffect(() => {
     setHomeUsers(users);
   }, [users]) 
 
+  const handleClose = () => {
+    setDialogOpen(false); 
+  }
+  const openNameEntryDialog = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode == 13) {
+      setDialogOpen(true);
+    }
+  }
+
+  const submitNewUser = () => {
+    history.push('/clock-in-page');
+  }
+
   return (
     <div> 
       <NameInputRow>
-        <Input />
+        {/* add filtering of list */}
+        <Input
+          onKeyDown={openNameEntryDialog}
+          onChange={(e: any) => setNewName(e.currentTarget.value)}
+          inputProps={{
+            style: { textAlign: 'center' }
+          }} 
+          placeholder="new user"
+
+        />
       </NameInputRow>
       <UserList>
       {homeUsers.map((u) => {
@@ -55,6 +83,15 @@ const Home: React.FunctionComponent<Props> = ({ users }) => {
         )
       })}
       </UserList>
+      <Dialog onClose={handleClose} open={dialogIsOpen}>
+        <DialogTitle> Would you like to enter as a new user? </DialogTitle>
+        <DialogContent style={{textAlign: 'center'}}>
+          {newName}
+        </DialogContent>
+        <DialogActions style={{ justifyContent: 'center' }}>
+          <UserButton onClick={submitNewUser}> OK </UserButton>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
